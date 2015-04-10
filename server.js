c1502db3af0052e961541bf679f8168fda38a2bf
Kevin Yang <yang.k@me.com>
2015-04-09 20:58:45 -0400

    forked passport+mean stack from jannunzi

--- server.js
+++ server.js
@@ -1,159 +1,163 @@
-#!/bin/env node
-//  OpenShift sample Node application
 var express = require('express');
-var fs      = require('fs');
+var app = express();
+var bodyParser = require('body-parser');
+var multer = require('multer'); 
+var passport      = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var cookieParser  = require('cookie-parser');
+var session       = require('express-session');
+var mongoose      = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/test');
 
+var UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    firstName: String,
+    lastName: String,
+    email: String,
+    roles: [String]
+});
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+var UserModel = mongoose.model('UserModel', UserSchema);
 
-    //  Scope.
-    var self = this;
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(multer()); // for parsing multipart/form-data
+app.use(session({ secret: 'this is the secret' }));
+app.use(cookieParser())
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use(express.static(__dirname + '/public'));
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
+//var users =
+//[
+//    {username: 'alice', password: 'alice', firstName: 'Alice', lastName: 'Wonderland', roles: ['admin', 'student', 'instructor']},
+//    {username: 'bob', password: 'bob', firstName: 'Bob', lastName: 'Marley', roles: ['student']},
+//    {username: 'charlie', password: 'charlie', firstName: 'Charlie', lastName: 'Brown', roles: ['instructor']}
+//];
 
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+passport.use(new LocalStrategy(
+function(username, password, done)
+{
+//    for(var u in users)
+//    {
+//        if(username == users[u].username && password == users[u].password)
+//        {
+//            return done(null, users[u]);
+//        }
+//    }
+    UserModel.findOne({username: username, password: password}, function(err, user)
+    {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        return done(null, user);
+    })
+}));
 
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
 
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
 
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
+app.post("/login", passport.authenticate('local'), function(req, res){
+    var user = req.user;
+    console.log(user);
+    res.json(user);
+});
+
+app.get('/loggedin', function(req, res)
+{
+    res.send(req.isAuthenticated() ? req.user : '0');
+});
+    
+app.post('/logout', function(req, res)
+{
+    req.logOut();
+    res.send(200);
+});     
+
+app.post('/register', function(req, res)
+{
+    var newUser = req.body;
+    newUser.roles = ['student'];
+    UserModel.findOne({username: newUser.username}, function(err, user)
+    {
+        if(err) { return next(err); }
+        if(user)
+        {
+            res.json(null);
+            return;
         }
-
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
-
-
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
+        var newUser = new UserModel(req.body);
+        newUser.save(function(err, user)
+        {
+            req.login(user, function(err)
+            {
+                if(err) { return next(err); }
+                res.json(user);
+            });
         });
-    };
+    });
+});
 
+var auth = function(req, res, next)
+{
+    if (!req.isAuthenticated())
+        res.send(401);
+    else
+        next();
+};
 
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
+app.get("/rest/user", auth, function(req, res)
+{
+    UserModel.find(function(err, users)
+    {
+        res.json(users);
+    });
+});
 
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
+app.delete("/rest/user/:id", auth, function(req, res){
+    UserModel.findById(req.params.id, function(err, user){
+        user.remove(function(err, count){
+            UserModel.find(function(err, users){
+                res.json(users);
+            });
         });
-    };
+    });
+});
 
-};   /*  Sample Application.  */
+app.put("/rest/user/:id", auth, function(req, res){
+    UserModel.findById(req.params.id, function(err, user){
+        user.update(req.body, function(err, count){
+            UserModel.find(function(err, users){
+                res.json(users);
+            });
+        });
+    });
+});
 
+app.post("/rest/user", auth, function(req, res){
+    UserModel.findOne({username: req.body.username}, function(err, user) {
+        if(user == null)
+        {
+            user = new UserModel(req.body);
+            user.save(function(err, user){
+                UserModel.find(function(err, users){
+                    res.json(users);
+                });
+            });
+        }
+        else
+        {
+            UserModel.find(function(err, users){
+                res.json(users);
+            });
+        }
+    });
+});
 
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
-
+app.listen(3000);
