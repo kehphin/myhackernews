bc6833cb7fd393e131f56470ec0f84ad1c838846
Kevin Yang <yang.k@me.com>
2015-04-16 01:13:36 -0400

    sort by new or top, combined url and title, fixed out of order bug

--- public/js/app.js
+++ public/js/app.js
@@ -31,17 +31,28 @@ app.factory('StoryService', function StoryService($q, $http) {
 
     var favorites = [];
 
-    var getTopStories = function() {
+    var getStories = function(sortType) {
       var deferred = $q.defer();
 
-      $http.get("https://hacker-news.firebaseio.com/v0/topstories.json").success(function(json) {
-        var topStoryList = [];
-        for (var i=0; i<20; i++) {
-          $http.get("https://hacker-news.firebaseio.com/v0/item/" + json[i] + ".json").success(function(story) {
-            topStoryList.push(story);
+      // Get array of IDs of the top 20 stories
+      $http.get("https://hacker-news.firebaseio.com/v0/" + sortType + ".json").success(function(json) {
+        var stories = {};
+        var storyCounter = 0; // keep track of when we reach 20 stories in the story list so we can resolve the deferred
 
-            if(topStoryList.length == 20) {
-              deferred.resolve(topStoryList);
+        // make each individual GET request (get story by ID)
+        for (var i=0; i<20; i++) {
+          $http.get("https://hacker-news.firebaseio.com/v0/item/" + json[i] + ".json?").success(function(story) {
+            stories[story.id.toString()] = story;
+            storyCounter++;
+
+            if(storyCounter == 20) {
+              var storyList = [];
+              $.each(json, function(index, storyId) {
+                if (index == 20) return false; // break out of loop after first 20 stories
+                storyList.push(stories[storyId.toString()]);
+              });
+
+              deferred.resolve(storyList);
             }
           });
         }
@@ -61,7 +72,7 @@ app.factory('StoryService', function StoryService($q, $http) {
     }
 
     return {
-        getTopStories: getTopStories,
+        getStories: getStories,
         addToFavorites: addToFavorites,
         getFavorites: getFavorites
     };
--- public/views/home/home.js
+++ public/views/home/home.js
@@ -1,14 +1,17 @@
 app.controller("HomeCtrl", function($scope, $http, $location, $rootScope, StoryService) {
   $scope.init = function() {
-    StoryService.getTopStories().then(function (stories) {
+    StoryService.getStories("topstories").then(function(stories) {
       $scope.stories = stories;
     });
+  }
 
+  $scope.sortStories = function(sortType) {
+    StoryService.getStories(sortType).then(function(stories) {
+      $scope.stories = stories;
+    });
   }
 
   $scope.addToFavorites = function(story) {
     StoryService.addToFavorites(story);
   }
-
-
 });
