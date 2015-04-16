78392f91e7229397d63f8a963b4f0cce7d3d2cda
Kevin Yang <yang.k@me.com>
2015-04-16 18:16:29 -0400

    added support for comments

--- public/views/article/article.js
+++ public/views/article/article.js
@@ -1,7 +1,7 @@
 app.controller('ArticleCtrl', function($scope, $http, $rootScope, $routeParams, StoryService) {
     $scope.$on('$viewContentLoaded', function() {
-
         StoryService.getStory($routeParams.articleId).then(function(story) {
+
             $scope.article = story;
         });
 
@@ -11,8 +11,32 @@ app.controller('ArticleCtrl', function($scope, $http, $rootScope, $routeParams, 
         });
 
         $http.get('/api/article/' + $routeParams.articleId + "/usersFavorited").success(function(articleFavorited) {
-            console.log(articleFavorited.users);
             $scope.articleFavorited = articleFavorited.users;
         });
     });
+
+    $scope.postComment = function() {
+        var commentBody = {
+            article: {
+                author: $scope.article.by,
+                descendants: $scope.article.descendants,
+                HNId: $scope.article.id,
+                score: $scope.article.score,
+                time: $scope.article.time,
+                title: $scope.article.title,
+                type: $scope.article.type,
+                url: $scope.article.url
+            },
+            comment: {
+                poster: $rootScope.currentUser.username,
+                article: $scope.article.id,
+                text: $scope.commentText
+            }
+        };
+
+        $http.post('/api/article/' + $scope.article.id + "/comment", commentBody).success(function(users) {
+            console.log("successfully posted comment");
+            $scope.comments.push($scope.commentText);
+        });
+    };
 });
