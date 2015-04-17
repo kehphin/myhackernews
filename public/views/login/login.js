a06b89fc8e4b850a86f2801f3d313c7437e1105d
Kevin Yang <yang.k@me.com>
2015-04-16 21:09:37 -0400

    bug fixes for login and register, removed ability to delete another user's favorites

--- public/views/login/login.js
+++ public/views/login/login.js
@@ -1,8 +1,13 @@
 app.controller("LoginCtrl", function($scope, $http, $location, $rootScope){
-    $scope.login = function(user){
-        $http.post("/api/login", user).success(function(response){
-          $rootScope.currentUser = response;
-          $location.url("/profile/" + user.username);
-        });
-    }
+  $scope.login = function(user){
+    $http.post("/api/login", user).success(function(response){
+      $rootScope.currentUser = response;
+      $location.url("/profile/" + user.username);
+    }).error(function(data, status, headers, config) {
+      // handle case where user tries to login with an invalid password
+      if (status == 401) {
+        $scope.message = "Invalid password";
+      }
+    });
+  }
 });
--- public/views/register/register.js
+++ public/views/register/register.js
@@ -1,21 +1,21 @@
 app.controller("RegisterCtrl", function($scope, $http, $location, $rootScope){
-    $scope.register = function(user){
-        console.log(user);
-        if(user.password != user.password2 || !user.password || !user.password2)
-        {
-            $rootScope.message = "Your passwords don't match";
+  $scope.register = function(user){
+    console.log(user);
+    if(user.password != user.password2 || !user.password || !user.password2) {
+      $scope.message = "Your passwords don't match";
+    } else {
+      $http.post("/api/user", user).success(function(response) {
+        console.log(response);
+        if(response != null) {
+          $rootScope.currentUser = response;
+          $location.url("/profile/" + $rootScope.currentUser.username);
         }
-        else
-        {
-            $http.post("/api/user", user)
-            .success(function(response){
-                console.log(response);
-                if(response != null)
-                {
-                    $rootScope.currentUser = response;
-                    $location.url("/profile/" + $rootScope.currentUser.username);
-                }
-            });
+      }).error(function(data, status, headers, config) {
+        // handle case where user tries to register for a username that already exists
+        if (status == 500) {
+          $scope.message = "Username already exists";
         }
+      });
     }
+  }
 });
